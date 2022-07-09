ed73475f76f2c8d724871d61516c4827c50c7b69
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-09 15:40:57 +0000

    Day one

---
+++ DAY-ONE/Exercise-3_input-function.py
@@ -0,0 +1,5 @@
+'''Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.'''
+
+character = str(input("What is your name?:"))
+print("The number of character in your name is :\n")
+print(len(character))
---
+++ DAY-ONE/Exercise-4_Variables.py
@@ -0,0 +1,18 @@
+'''Write a program that switches the values stored in the variables a and b.'''
+
+a = input("a: ")
+b = input("b: ")
+# íº¨ Don't change the code above í±†
+
+####################################
+#Write your code below this line í±‡
+c = a
+a = b
+b = c
+
+#Write your code above this line í±†
+####################################
+
+# íº¨ Don't change the code below í±‡
+print("a: " + a)
+print("b: " + b)
