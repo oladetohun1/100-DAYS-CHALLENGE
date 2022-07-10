c1c7f417286dd9921a36043fef796426c31b5155
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-07-10 22:59:08 +0000

    done

---
+++ DAY-ONE/project-brand_name-generator.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")
+city = str(input("what is the name of the city you grew up in\n"))
+pet = str(input("What's your pet's name?\n"))
+print("Your band name could be",city, pet)
